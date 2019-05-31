5834866636517b0d325acf188fe71a20f8cbde36
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-31 12:07:51 -0500

    projects all working

--- data/projects/projectRouter.js
+++ data/projects/projectRouter.js
@@ -2,32 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
-const projectDb = require('../helpers/projectModel.js'); //question if this is right/
-const actionDb = require('../helpers/actionModel.js');//question if this is right?
+const projectDb = require('../helpers/projectModel.js'); 
+const actionDb = require('../helpers/actionModel.js');
 
 
 
+//working
 router.get('/:id', (req, res) => {
-    console.log(req.params.id)
+   
     const id = req.params.id;
-    projectDb
-    .get(id)
-    .then(res => {
-        res.status(200).json(res)
+    projectDb.get(id).then(response => {
+        res.json(response)
     })
     .catch(error => {
-        res.status(500).json({message: "internal server error"})
+        console.error(error)
     })
 })
 
 //working
 router.post('/', (req, res) => {
     const project = { name: req.body.name, description: req.body.description};
-    console.log(project)
     projectDb
     .insert(project)
     .then(result => {
-        console.log(result)
         res.status(200).json(result)
     })
     .catch(error => {
@@ -35,41 +32,31 @@ router.post('/', (req, res) => {
     })
 })
 
-
+//working
 router.put('/:id', (req,res) => {
     const changes = {...req.body}
     projectDb
     .update(req.params.id, changes)
-    .then(res => {
-        res.status(200).json(res)
+    .then(result => {
+        res.json(result)
     })
     .catch(error => {
         res.status(500).json({messge: "internal server error"})
     } )
 })
 
+//working
 router.delete('/:id', (req,res) => {
     projectDb
     .remove(req.params.id)
-    .then(res => {
-        res.status(200).json({messge: "internal server error"})
+    .then(result => {
+        res.status(200).json(result)
     })
     .catch(error => {
         res.status(500).json({message: "internal server error"})
     })
 })
 
-// router.get('/:id', (req, res) => {
-//     console.log(req.params.id)
-//     projectDb
-//     .getProjectActions(req.params.id)
-//     .then(res => {
-//         res.status(200).json(res)
-//     })
-//     .catch(error => {
-//         res.status(500).json({message: "internal server error"})
-//     })
-// })
 
 
 
